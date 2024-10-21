27e88b126691ccb40f6506756656e333d6e3c219
Vandana-3 <112668987+Vandana-3@users.noreply.github.com>
2024-10-21 11:48:43 -0700

    Update app.py

--- Demo/app.py
+++ Demo/app.py
@@ -28,7 +28,7 @@ df = df.dropna(subset=['Application_Date'])
 #################### Load Model #######################
 # with open('rf_model.pkl', 'rb') as file:
 # Set the path to the random forest model (since it's in the same directory)
-model_path = os.path.join(os.path.dirname(__file__), 'Demo/rf_model.pkl')
+model_path = os.path.join(os.path.dirname(__file__), 'Demo/models/rf_model.pkl')
 
 # Initialize model handler and load the model
 model_handler = ModelHandler()
